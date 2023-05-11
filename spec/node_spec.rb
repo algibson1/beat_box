d199e550c4bc295d196a0ec756768ad0319ad33f
Artemy Gibson <artemygibson@gmail.com>
2023-05-11 16:09:34 -0700

    Test that node returns data

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -8,4 +8,8 @@ describe Node do
     it 'exists' do
         expect(node).to be_a(Node)
     end
+    it 'has data' do
+        expect(node.data).to eq("plop")
+        expect(node.next_node).to eq(nil)
+    end
 end
