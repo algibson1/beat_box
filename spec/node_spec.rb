1c601054eb604ab950f2e957bc64e641de9e9093
Artemy Gibson <artemygibson@gmail.com>
2023-05-11 16:13:23 -0700

    Edit syntax errors

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,14 +1,15 @@
-require ".lib/node"
+require "./lib/node"
 require "rspec"
 
 describe Node do
-    before do
-        node = Node.new("plop")
-    end
     it 'exists' do
+        node = Node.new("plop")
+
         expect(node).to be_a(Node)
     end
     it 'has data' do
+        node = Node.new("plop")
+
         expect(node.data).to eq("plop")
         expect(node.next_node).to eq(nil)
     end
