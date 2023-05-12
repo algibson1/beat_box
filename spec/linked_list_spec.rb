ac02a89bec7d4d870b8981aa24049a0b040fd856
Artemy Gibson <artemygibson@gmail.com>
2023-05-11 20:38:52 -0700

    Add tests for find, include, and pop methods

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -97,20 +97,33 @@ describe LinkedList do
         list.prepend("dop")
         expect(list.to_string).to eq("dop")
     end
-    xit 'can insert nodes' do
+    it 'can insert nodes' do
         list = LinkedList.new
         list.append("plop")
         list.append("suu")
         list.prepend("dop")
         list.insert(1, "woo")
-        expect(list.to_string).to eq("dop woo suu plop")
+        expect(list.to_string).to eq("dop woo plop suu")
     end
-    xit 'can insert nodes to empty list' do
+    it 'can insert nodes part two (longer list)' do
         list = LinkedList.new
-        list.insert(1, "woo")
+        list.append("plop")
+        list.append("suu")
+        list.prepend("dop")
+        list.append("beep")
+        list.append("hop")
+        list.append("boop")
+        list.append("deep")
+        list.append("doop")
+        list.insert(5, "woo")
+        expect(list.to_string).to eq("dop plop suu beep hop woo boop deep doop")
+    end
+    it 'can insert nodes to empty list' do
+        list = LinkedList.new
+        list.insert(3, "woo")
         expect(list.to_string).to eq("woo")
     end
-    xit 'adds inserted nodes to end when number is greater than count' do
+    it 'adds inserted nodes to end when number is greater than count' do
         list = LinkedList.new
         list.append("plop")
         list.append("suu")
@@ -118,11 +131,99 @@ describe LinkedList do
         expect(list.to_string).to eq("plop suu woo")
         expect(list.count).to eq(3)
     end
-    xit 'can prepend using insert' do
+    it 'can prepend using insert' do
         list = LinkedList.new
         list.append("plop")
         list.append("suu")
         list.insert(0, "woo")
-        expect(list.to_string).to eq("wood plop suu")
+        expect(list.to_string).to eq("woo plop suu")
+    end
+    xit 'can find node data' do
+        list = LinkedList.new
+        list.append("deep")
+        list.append("woo")
+        list.append("shi")
+        list.append("shu")
+        list.append("blop")
+        expect(list.to_string).to eq("deep woo shi shu blop")
+        expect(list.find(2,1)).to eq("shi")
+        expect(list.find(1,3)).to eq("woo shi shu")
+    end
+    xit 'returns nil if find is called on empty list' do
+        list = LinkedList.new
+        expect(list.find(2,1)).to be_nil
+    end
+    xit 'returns nil if node position in find does not exist' do
+        list = LinkedList.new
+        list.append("deep")
+        list.append("woo")
+        list.append("shi")
+        list.append("shu")
+        list.append("blop")
+        expect(list.find(7,1)).to be_nil
+    end
+    xit 'returns all trailing nodes even if find quantity is too high' do
+        list = LinkedList.new
+        list.append("deep")
+        list.append("woo")
+        list.append("shi")
+        list.append("shu")
+        list.append("blop")
+        expect(list.find(2, 7)).to eq("shi shu blop")
+    end
+    xit 'position 0 works as an argument in find method' do
+        list = LinkedList.new
+        list.append("deep")
+        list.append("woo")
+        list.append("shi")
+        list.append("shu")
+        list.append("blop")
+        expect(list.find(0, 2)).to eq("deep woo")
+    end
+    xit 'can see if nodes include certain data' do
+        list = LinkedList.new
+        list.append("deep")
+        list.append("woo")
+        list.append("shi")
+        list.append("shu")
+        list.append("blop")
+        expect(list.includes?("deep")).to be true
+        expect(list.includes?("dep")).to be false
+    end
+    xit 'does not error when include method is called on empty list' do
+        list = LinkedList.new
+        expect(list.includes?("deep")).to be false
+    end
+    xit 'checks for full node data' do
+        list = LinkedList.new
+        list.append("deep")
+        list.append("woo")
+        list.append("shi")
+        list.append("shu")
+        list.append("blop")
+        expect(list.includes?("deep")).to be true
+        expect(list.includes?("dee")).to be false
+    end
+    xit 'pops trailing node from list' do
+        list = LinkedList.new
+        list.append("deep")
+        list.append("woo")
+        list.append("shi")
+        list.append("shu")
+        list.append("blop")
+        expect(list.pop).to eq("blop")
+        expect(list.pop).to eq("shu")
+        expect(list.to_string).to eq("deep woo shi")
+    end
+    xit 'can pop head if it is the only node' do
+        list = LinkedList.new
+        list.append("deep")
+        expect(list.pop).to eq("deep")
+        expect(list.head).to be_nil
+        expect(list.count).to eq(0)
+    end
+    xit 'cannot pop anything if list is empty' do
+        list = LinkedList.new
+        expect(list.pop).to be_nil
     end
 end
