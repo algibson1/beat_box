00e57500dfba68851750fdab720a8deb0b3de8dd
Artemy Gibson <artemygibson@gmail.com>
2023-05-11 17:06:07 -0700

    Test LinkedList appends multiple nodes

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -10,21 +10,32 @@ describe LinkedList do
         list = LinkedList.new
         expect(list.head).to be_nil
     end
-    it 'appends data using nodes' do
+    it 'appends data from one node' do
         list = LinkedList.new
         list.append("doop")
         expect(list.head).to be_a(Node)
         expect(list.head.data).to eq("doop")
         expect(list.head.next_node).to be_nil
     end
-    it 'can count nodes' do
+    it 'can count a single node' do
         list = LinkedList.new
         list.append("doop")
         expect(list.count).to eq(1)
     end
-    it 'can turn node data into string' do
+    it 'can turn data into string from one node' do
         list = LinkedList.new
         list.append("doop")
         expect(list.to_string).to eq("doop")
+    end
+    it 'can append multiple nodes' do
+        list = LinkedList.new
+        expect(list.head).to be_nil
+        list.append("doop")
+        list.append("deep")
+        expect(list.head.next_node).to be_a(Node)
+        expect(list.head.next_node.data).to eq("deep")
+        expect(list.head.next_node.next_node).to be_nil
+        list.append("bop")
+        expect(list.head.next_node.next_node).to be_a(Node)
     end
 end
