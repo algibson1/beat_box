5902468384e05d0e9ac3ccab1c3150b1d6dcf3de
Artemy Gibson <artemygibson@gmail.com>
2023-05-12 18:58:43 -0700

    Test append and count methods

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -9,4 +9,18 @@ describe BeatBox do
         expect(bb).to be_a(BeatBox)
         expect(bb.list).to be_a(LinkedList)
     end
+    it 'can append data' do
+        bb = BeatBox.new
+        expect(bb.list.head).to be_nil
+        expect(bb.append("deep doo ditt")).to eq("deep doo ditt")
+        expect(list.head.data).to eq("deep")
+        expect(bb.list.head.next_node.data).to eq("doo")
+        expect(bb.append("woo hoo shu")).to eq("woo hoo shu")
+    end
+    it 'can count nodes' do
+        bb = BeatBox.new
+        bb.append("deep doo ditt")
+        bb.append("woo hoo shu")
+        expect(bb.count).to eq(6)
+    end
 end
