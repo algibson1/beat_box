ac4b296e88131468e050471d44702450e1c8cc4c
Artemy Gibson <artemygibson@gmail.com>
2023-05-12 19:00:14 -0700

    Add append and count methods

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -3,4 +3,14 @@ class BeatBox
     def initialize
         @list = LinkedList.new
     end
+    def append(string)
+        sounds = string.split
+        sounds.each do |data|
+            @list.append(data)
+        end
+        string
+    end
+    def count
+        @list.count
+    end
 end
