cdf8d26062eb9cc665d2a78beda07551fbe15c9e
Artemy Gibson <artemygibson@gmail.com>
2023-05-14 11:00:26 -0700

    Add prepend and all methods, and reject invalid beats

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -1,12 +1,14 @@
 class BeatBox
     attr_reader :list
-    def initialize
+    def initialize(string = nil)
         @list = LinkedList.new
+        @valid_beats = ["tee", "tee", "doo", "hoo", "ditt", "deep", "bop", "boop", "la", "na", "doop", "plop", "suu", "dop", "woo", "hop", "shi", "shu", "blop"]
+        append(string) if string
     end
     def append(string)
         sounds = string.split
         sounds.each do |data|
-            @list.append(data)
+            @list.append(data) if @valid_beats.include?(data)
         end
         string
     end
@@ -15,6 +17,16 @@ class BeatBox
     end
     def play
         `say -r 150 -v Boing #{@list.to_string}`
+    end
+    def prepend(string)
+        sounds = string.split.reverse
+        sounds.each do |data|
+            @list.prepend(data) if @valid_beats.include?(data)
+        end
+        string
+    end
+    def all
+        @list.to_string
     end
 end
 
