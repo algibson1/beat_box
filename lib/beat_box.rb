c424c12bc3563157ce4932efef038dc407f33ad4
Artemy Gibson <artemygibson@gmail.com>
2023-05-14 11:27:28 -0700

    Add voice and rate set and reset methods

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -1,7 +1,10 @@
 class BeatBox
     attr_reader :list
+    attr_accessor :voice, :rate
     def initialize(string = nil)
         @list = LinkedList.new
+        @voice = "Boing"
+        @rate = 500
         @valid_beats = ["tee", "tee", "doo", "hoo", "ditt", "deep", "bop", "boop", "la", "na", "doop", "plop", "suu", "dop", "woo", "hop", "shi", "shu", "blop"]
         append(string) if string
     end
@@ -16,7 +19,8 @@ class BeatBox
         @list.count
     end
     def play
-        `say -r 150 -v Boing #{@list.to_string}`
+        `say -r #{@rate} -v #{@voice} #{@list.to_string}`
+        @list.count
     end
     def prepend(string)
         sounds = string.split.reverse
@@ -27,6 +31,12 @@ class BeatBox
     end
     def all
         @list.to_string
+    end
+    def reset_voice
+        @voice = "Boing"
+    end
+    def reset_rate
+        @rate = 500
     end
 end
 
